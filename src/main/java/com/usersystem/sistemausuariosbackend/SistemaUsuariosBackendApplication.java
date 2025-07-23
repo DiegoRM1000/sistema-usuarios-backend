003529c719bfb1bb18f3dfe4ada95c59ddf6e683
Dev Diego Rivera <diegoriveramedina3@gmail.com>
2025-07-23 16:59:39 -0500

    Primer commit: Creacion de proyecto y base de datos

---
+++ src/main/java/com/usersystem/sistemausuariosbackend/SistemaUsuariosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.usersystem.sistemausuariosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaUsuariosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaUsuariosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usersystem/sistemausuariosbackend/SistemaUsuariosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usersystem.sistemausuariosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaUsuariosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
